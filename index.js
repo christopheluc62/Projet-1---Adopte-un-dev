5a302dd7d6a60b161ef4a6fd09aacd5bde7442d4
Kévin <bdtkevin@gmail.com>
2021-03-13 18:22:44 +0100

    added 1st version of the adding devs part

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ checkBurger.addEventListener("change", function() {
     let options = document.getElementById("options");
     this.checked ? options.className = "optDisplay" : options.className = "optHide";
 });
+
+let checkCaddy = document.querySelector("#checkCaddy");
+checkCaddy.addEventListener("change", function() {
+    let options = document.getElementById("added");
+    this.checked ? options.className = "addDisplay" : options.className = "addHide";
+});
