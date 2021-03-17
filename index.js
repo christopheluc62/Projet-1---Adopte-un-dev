511b5ecfde4c2287d1508516eafe5051c85cd66e
Kévin <bdtkevin@gmail.com>
2021-03-17 22:19:36 +0100

    link github & linkedin added - array of cards created

--- index.js
+++ index.js
@@ -1,5 +1,48 @@
 const modal = document.querySelector(".modal");
-const sendMail = document.querySelector(".iconSendMail");
+const iconsMail = document.getElementsByClassName("iconSendMail");
+
+const cardsList = [
+    {
+        firstName: "Antoine",
+        lastName: "Cau",
+        img: "./pictures/antoine.jpg",
+        techs: ["html", "css", "github"],
+        gitHub: "https://github.com/AntoineCAU",
+        linkedin: "https://www.linkedin.com/in/antoine-cau-dw/"
+    },
+    {
+        firstName: "Caroline",
+        lastName: "Dorchies",
+        img: "./pictures/caro.jpg",
+        techs: ["html", "css", "js"],
+        gitHub: "https://github.com/AntoineCAU",
+        linkedin: "https://www.linkedin.com/in/antoine-cau-dw/"
+    },
+    {
+        firstName: "Christophe",
+        lastName: "Luc",
+        img: "./pictures/christophe.jpg",
+        techs: ["github"],
+        gitHub: "https://github.com/AntoineCAU",
+        linkedin: "https://www.linkedin.com/in/antoine-cau-dw/"
+    },
+    {
+        firstName: "Kévin",
+        lastName: "Bodart",
+        img: "./pictures/kevin.jpg",
+        techs: ["html", "css", "github"],
+        gitHub: "https://github.com/AntoineCAU",
+        linkedin: "https://www.linkedin.com/in/antoine-cau-dw/"
+    },
+    {
+        firstName: "Mickael",
+        lastName: "Dessin",
+        img: "./pictures/mika.jpg",
+        techs: ["html", "css", "github"],
+        gitHub: "https://github.com/AntoineCAU",
+        linkedin: "https://www.linkedin.com/in/antoine-cau-dw/"
+    }
+];
 
 // close modal if click ext
 modal.addEventListener("click", () => {
@@ -7,6 +50,8 @@ modal.addEventListener("click", () => {
 });
 
 // open modal when click send mail icon
-sendMail.addEventListener("click", () => {
-    modal.style.display = "flex";
-});
+for (let iconMail of iconsMail) {
+    iconMail.addEventListener("click", () => {
+        modal.style.display = "flex";
+    });
+};
