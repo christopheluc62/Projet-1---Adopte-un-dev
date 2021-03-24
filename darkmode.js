6a3cdfbe6af6461b74bbfbf8e7a66ba12b6755fe
mickaeldessin <mickaeldessin@outlook.fr>
2021-03-24 18:38:28 +0100

    dark mode nav barre

--- darkmode.js
+++ darkmode.js
@@ -1,5 +1,11 @@
 let theme_toggler = document.querySelector('#theme_toggler');
-
+ let count = 1;
 theme_toggler.addEventListener('click', function(){ 
     document.body.classList.toggle('dark_mode');
+    count ++;
+    if(count %2 === 0){
+      document.getElementById("logoAdopt").src = "pictures/DMDESKTOP.png";
+    }else { 
+      document.getElementById("logoAdopt").src= "pictures/logoAdopteUnDev.png";
+      }
 });
