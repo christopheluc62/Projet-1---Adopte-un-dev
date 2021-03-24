35f3d18f2332e9bdebd45016d481e71c49677055
CaroD59 <dorchies.c@gmail.com>
2021-03-24 18:43:53 +0100

    button dark mode modif

--- darkmode.js
+++ darkmode.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 let theme_toggler = document.querySelector('.theme_toggler');
 
 theme_toggler.addEventListener('click', function(){ 
@@ -11,16 +10,4 @@ const chk = document.getElementById('chk');
 
 chk.addEventListener('change', () => {
 	document.body.classList.toggle('dark');
-=======
-let theme_toggler = document.querySelector('#theme_toggler');
- let count = 1;
-theme_toggler.addEventListener('click', function(){ 
-    document.body.classList.toggle('dark_mode');
-    count ++;
-    if(count %2 === 0){
-      document.getElementById("logoAdopt").src = "pictures/DMDESKTOP.png";
-    }else { 
-      document.getElementById("logoAdopt").src= "pictures/logoAdopteUnDev.png";
-      }
->>>>>>> ddee5a8f0767b23540f08df41c09ddd06cb51b0f
 });
