344df56b3437690fd172ec6c882ca73a7dbe2340
mickaeldessin <mickaeldessin@outlook.fr>
2021-03-24 19:04:20 +0100

    add darkmode JS

--- darkmode.js
+++ darkmode.js
@@ -1,7 +1,13 @@
-let theme_toggler = document.querySelector('.theme_toggler');
-
+let theme_toggler = document.querySelector('#chk');
+ let count = 1;
 theme_toggler.addEventListener('click', function(){ 
     document.body.classList.toggle('dark_mode');
+    count ++;
+    if(count %2 === 0){
+      document.getElementById("logoAdopt").src = "pictures/DMDESKTOP.png";
+    }else { 
+      document.getElementById("logoAdopt").src= "pictures/logoAdopteUnDev.png";
+      }
 });
 
 
